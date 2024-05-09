b4e585d09b81cd3e178f5d030b6907669d3c00b5
mike-terrible <41124780+mike-terrible@users.noreply.github.com>
2024-05-09 13:14:09 +0300

    Add files via upload

---
+++ gad-onType.cpp
@@ -0,0 +1,44 @@
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "gad.h"
+
+using namespace Gad;
+
+
+char* MyRT::onType(char* xtype) {
+  static char zv[128];
+  //const char* Str[] = {"string","строка",nullptr };
+  if(cmp(xtype,Str)) {
+    if(gen == GO) strcpy(zv,"string");
+    if(gen == RUST) strcpy(zv,"&str");
+    if(gen == MOJO) strcpy(zv,"String");
+    return zv;
+  };
+  //const char* Num[] =  { "integer","int","число","цел",nullptr };
+  if(cmp(xtype, Num )) {
+    if(gen == RUST) strcpy(zv,"i64");
+    if(gen == GO) strcpy(zv,"int");
+    if(gen == MOJO) strcpy(zv,"Int");
+    return zv;
+  };
+  //const char* Real[] = { "real","вещ", nullptr }; 
+  if(cmp(xtype, Real)) {
+    if(gen == RUST) strcpy(zv,"f64");
+    if(gen == GO) strcpy(zv,"float64");
+    if(gen == MOJO) strcpy(zv,"Float64");
+    return zv;
+  };
+
+ //const char* Light[] = { "light","свеча","свет",nullptr };
+ if(cmp(xtype, Light )) {
+  if((gen == GO)||(gen == RUST)) strcpy(zv,"bool");
+  if(gen == MOJO) strcpy(zv,"Bool");
+  return zv;
+ };
+ zv[0]=0;
+ return zv;
+}
+
---
+++ gad-words.cpp
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "gad.h"
+
+namespace Gad { 
+  const char* On[] = { "on","зажечь",nullptr};
+  const char* Off[] = { "off", "потушить", nullptr };
+  const char* Comment[] = { "исполать","донос","(*" , nullptr };
+  const char* EndComment[] = { "зри","*)", nullptr };
+  const char* Aka[] = { ":","as","aka","как", nullptr };
+  const char* With[] = { "with","для", nullptr };
+  const char* Return[] = { "result" , "exit", "ход", "дать", nullptr  };
+  const char* When[] = { "when", "когда" , nullptr };
+  const char* Repeat[] = { "repeat","повтор", nullptr };
+  const char* Sic[] = {"sic","lay","here","вот", nullptr };
+  const char* Else[] = { "?-", "else", "иначе", "погасло",nullptr };
+  const char* Then[] = { "?+","then","ли", "тогда",nullptr };
+  const char* If[] = { "?!","if","если","горит",nullptr };
+  const char* Show[] = { "show", "показать",nullptr };
+  const char* Skrepa[] = { "mess","скрепа", "скрижаль", "грамота",nullptr };
+  const char* Give[] = { "give", "!!!", "дать",nullptr };
+  const char* Job[] = { "@@","job", "call", "начать",nullptr };
+  const char* Amen[] = { "amen", "done", "loop", "аминь", "весть" , "опять",nullptr };
+  const char* Loop[] = { "loop", "опять", nullptr };
+  const char* Done[] = { "done", "весть", nullptr };
+  const char* Delo[] = { "proc","procedure", "десница","дело",nullptr };
+  const char* Declare[] = { "dcl","declare","пусть","да",nullptr};
+  const char* Is[] = { "is" , "суть", nullptr };
+  const char* Pora[] = {"???","execute","exec","start","run","пора" ,nullptr};
+
+}
