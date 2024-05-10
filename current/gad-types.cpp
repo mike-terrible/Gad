8e3a89bbefb3ae7de73568f54d9b92b7445378c1
mike-terrible <41124780+mike-terrible@users.noreply.github.com>
2024-05-10 03:30:49 +0300

    Add files via upload

--- current/gad-types.cpp
+++ current/gad-types.cpp
@@ -4,8 +4,8 @@
 #include "gad.h"
 
 namespace Gad {
-  const char* Str[] = {"string","строка",nullptr };
+  const char* Str[] = {"string","строка", "текст" ,nullptr };
   const char* Num[] = { "integer","int","num","число","цел",nullptr };
-  const char* Real[] = { "real","вещ", nullptr };
-  const char* Light[] = { "light","свеча","свет",nullptr };
+  const char* Real[] = { "real","вещ", "дій", nullptr };
+  const char* Light[] = { "light","свеча","свет","свічка", nullptr };
 }
--- current/gad-words.cpp
+++ current/gad-words.cpp
@@ -4,29 +4,35 @@
 #include "gad.h"
 
 namespace Gad { 
-  const char* On[] = { "on","зажечь",nullptr};
-  const char* Off[] = { "off", "потушить", nullptr };
-  const char* Comment[] = { "исполать","донос","(*" , nullptr };
-  const char* EndComment[] = { "зри","*)", nullptr };
-  const char* Aka[] = { ":","as","aka","как", nullptr };
-  const char* With[] = { "with","для", nullptr };
-  const char* Return[] = { "result" , "exit", "ход", "дать", nullptr  };
-  const char* When[] = { "when", "когда" , nullptr };
+  const char* Declare[] = { "dcl","declare","пусть","да", "хай" , "ну" , nullptr };
+  const char* Aka[] = { ":","as","aka","как", "таке", "це" , nullptr };
+  
+  const char* If[] = { "?!","if","если","горит", "чи", nullptr };
+  const char* Then[] = { "?+","then","ли", "тогда", "є" , nullptr };
+  const char* Else[] = { "?-", "else", "иначе", "погасло", "нема" ,nullptr };
+  const char* Done[] = { "?!)","done", "весть", "взгляд", "погляд" , nullptr };
+  
+  const char* When[] = { "when", "когда" , "коли" , nullptr };
   const char* Repeat[] = { "repeat","повтор", nullptr };
-  const char* Sic[] = {"sic","lay","here","вот", nullptr };
-  const char* Else[] = { "?-", "else", "иначе", "погасло",nullptr };
-  const char* Then[] = { "?+","then","ли", "тогда",nullptr };
-  const char* If[] = { "?!","if","если","горит",nullptr };
-  const char* Show[] = { "show", "показать",nullptr };
-  const char* Skrepa[] = { "mess","скрепа", "скрижаль", "грамота",nullptr };
-  const char* Give[] = { "give", "!!!", "дать",nullptr };
-  const char* Job[] = { "@@","job", "call", "начать",nullptr };
-  const char* Amen[] = { "amen", "done", "loop", "аминь", "весть" , "опять",nullptr };
-  const char* Loop[] = { "loop", "опять", nullptr };
-  const char* Done[] = { "done", "весть", nullptr };
-  const char* Delo[] = { "proc","procedure", "десница","дело",nullptr };
-  const char* Declare[] = { "dcl","declare","пусть","да",nullptr};
-  const char* Is[] = { "is" , "суть", nullptr };
-  const char* Pora[] = {"???","execute","exec","start","run","пора" ,nullptr};
+  const char* Loop[] = { "loop", "опять","далі", nullptr };
+  
+  const char* Job[] = { "@@","job", "call", "начать", "почати" ,nullptr };
+  const char* Give[] = { "give", "!!!", "дать", "дати", "кошик" , nullptr };
+  const char* Skrepa[] = { "mess","скрепа", "скрижаль", "грамота", "новина" , nullptr };
+  const char* Pora[] = {"???","execute","exec","start","run","пора","час", nullptr};
+  const char* Return[] = { "result" , "exit", "ход", "дать", "здобич" , nullptr  };
+  const char* Delo[] = { "proc","procedure", "десница","дело","справа",nullptr };
+  const char* Show[] = { "show", "показать", "вистава" , nullptr };
+  const char* Sic[] = {"sic","lay","here","вот", "вже",  nullptr };
+  const char* Is[] = { "is" , "суть", "докладно", nullptr };
+  const char* Amen[] = { "amen", "done", "аминь", "все", nullptr };
+  const char* With[] = { "with","для", "по", "ще" , nullptr };
+  
+  const char* On[] = { "on","зажечь", "запалити" ,nullptr};
+  const char* Off[] = { "off", "потушить", "загасити" , nullptr };
 
+  const char* Comment[] = { "исполать","донос","(*" , nullptr };
+  const char* EndComment[] = { "зри","*)" , nullptr };
+  
+   
 }
