bdac7d513953eb885712bc680dd8421b6ea2303e
mike-terrible <41124780+mike-terrible@users.noreply.github.com>
2024-05-30 13:39:22 +0300

    Add files via upload

---
+++ gad-files/ass.go
@@ -0,0 +1,18 @@
+package main
+  //   ass.гад  native assignment
+
+  func main() {
+    fuck()
+  }
+  func fuck() {
+    var a int
+
+    //!! goEval
+
+    //!! fromCalc
+    
+    a = 4
+
+        println(a)
+
+  }
